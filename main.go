81e97a161a78b04b9b0578cf3bfdac4135d3939a
LempekPL <Superlempek2004@gmail.com>
2021-04-04 23:24:58 +0200

    winning conditions, player change between games and points

--- main.go
+++ main.go
@@ -12,52 +12,72 @@ import (
 	"image/color"
 	_ "image/png"
 	"log"
+	"math/rand"
 	"os"
+	"time"
 )
 
 const (
-	sWidth   = 480
-	sHeight  = 600
-	fontSize = 15
-	dpi      = 72
+	sWidth      = 480
+	sHeight     = 600
+	fontSize    = 15
+	bigFontSize = 100
+	dpi         = 72
 )
 
 var (
-	mplusNormalFont font.Face
-	boardImage      = ebiten.NewImage(sWidth, sHeight)
-	symbolImage     *ebiten.Image
-	gameImage       = ebiten.NewImage(sWidth, sWidth)
+	normalText  font.Face
+	bigText     font.Face
+	boardImage  *ebiten.Image
+	symbolImage *ebiten.Image
+	gameImage   = ebiten.NewImage(sWidth, sWidth)
 )
 
 type Game struct {
 	playing   string
 	state     int
-	gameBoard [3][3]int
+	gameBoard [3][3]string
 	round     int
+	pointsO   int
+	pointsX   int
+	win       string
+	alter     int
 }
 
 func (g *Game) Update() error {
 	switch g.state {
 	case 0:
-		g.Load()
+		g.Init()
 		break
+
 	case 1:
 		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 			mx, my := ebiten.CursorPosition()
-			if mx/160 < 3 && mx >= 0 && my/160 < 3 && my >= 0 && g.gameBoard[mx/160][my/160] == 0 {
-				g.round++
-				if g.round%2 == 0 {
+			if mx/160 < 3 && mx >= 0 && my/160 < 3 && my >= 0 && g.gameBoard[mx/160][my/160] == "" {
+				if g.round%2 == 0+g.alter {
 					g.DrawSymbol(mx/160, my/160, "O")
-					g.gameBoard[mx/160][my/160] = 1
+					g.gameBoard[mx/160][my/160] = "O"
 					g.playing = "X"
 				} else {
 					g.DrawSymbol(mx/160, my/160, "X")
-					g.gameBoard[mx/160][my/160] = 2
+					g.gameBoard[mx/160][my/160] = "X"
 					g.playing = "O"
 				}
+				fmt.Println(g.round)
+				g.CheckWin()
+				g.round++
 			}
 		}
 		break
+	case 2:
+		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+			g.Load()
+		}
+		break
+	}
+	if inpututil.KeyPressDuration(ebiten.KeyR) == 60 {
+		g.Load()
+		g.ResetPoints()
 	}
 	if inpututil.KeyPressDuration(ebiten.KeyEscape) == 60 {
 		os.Exit(0)
@@ -72,16 +92,24 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	mx, my := ebiten.CursorPosition()
 
 	msgTFPS := fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.CurrentTPS(), ebiten.CurrentFPS())
-	text.Draw(screen, msgTFPS, mplusNormalFont, 0, sHeight-30, color.White)
+	text.Draw(screen, msgTFPS, normalText, 0, sHeight-30, color.White)
 	if inpututil.KeyPressDuration(ebiten.KeyEscape) > 1 {
 		msgClosing := fmt.Sprintf("CLOSING...")
 		colorChangeToExit := 255 - (255 / 60 * uint8(inpututil.KeyPressDuration(ebiten.KeyEscape)))
-		text.Draw(screen, msgClosing, mplusNormalFont, sWidth/2, sHeight-30, color.RGBA{R: 255, G: colorChangeToExit, B: colorChangeToExit, A: 255})
+		text.Draw(screen, msgClosing, normalText, sWidth/2, sHeight-30, color.RGBA{R: 255, G: colorChangeToExit, B: colorChangeToExit, A: 255})
+	} else if inpututil.KeyPressDuration(ebiten.KeyR) > 1 {
+		msgClosing := fmt.Sprintf("RESETING...")
+		colorChangeToReset := 255 - (255 / 60 * uint8(inpututil.KeyPressDuration(ebiten.KeyR)))
+		text.Draw(screen, msgClosing, normalText, sWidth/2, sHeight-30, color.RGBA{R: colorChangeToReset, G: 255, B: 255, A: 255})
 	}
-	msgXY := fmt.Sprintf("(%v, %v)", mx, my)
-	text.Draw(screen, msgXY, mplusNormalFont, sWidth/2, sHeight-5, color.White)
+	msgOX := fmt.Sprintf("O: %v | X: %v", g.pointsO, g.pointsX)
+	text.Draw(screen, msgOX, normalText, sWidth/2, sHeight-5, color.White)
+	if g.win != "" {
+		msgWin := fmt.Sprintf("%v wins!", g.win)
+		text.Draw(screen, msgWin, bigText, 0, sHeight/2, color.RGBA{G: 50, B: 200, A: 255})
+	}
 	msg := fmt.Sprintf("%v", g.playing)
-	text.Draw(screen, msg, mplusNormalFont, mx, my, color.RGBA{G: 255, A: 255})
+	text.Draw(screen, msg, normalText, mx, my, color.RGBA{G: 255, A: 255})
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -101,20 +129,60 @@ func (g *Game) DrawSymbol(x, y int, sym string) {
 	gameImage.DrawImage(symbolImage, opSymbol)
 }
 
-func (g *Game) Load() {
-
+func (g *Game) Init() {
 	var err error
 	boardImage, _, err = ebitenutil.NewImageFromFile("images/board.png")
 	if err != nil {
 		log.Fatal(err)
 	}
-	g.state = 1
-	g.gameBoard[0][0], g.gameBoard[0][1], g.gameBoard[0][2] = 0, 0, 0
-	g.gameBoard[1][0], g.gameBoard[1][1], g.gameBoard[1][2] = 0, 0, 0
-	g.gameBoard[2][0], g.gameBoard[2][1], g.gameBoard[2][2] = 0, 0, 0
-	g.playing = "O"
-	g.round = 1
+	re := newRandom().Intn(2)
+	if re == 0 {
+		g.playing = "O"
+		g.alter = 0
+	} else {
+		g.playing = "X"
+		g.alter = 1
+	}
+	g.Load()
+	g.ResetPoints()
+}
 
+func (g *Game) Load() {
+	gameImage.Clear()
+	g.gameBoard[0][0], g.gameBoard[0][1], g.gameBoard[0][2] = "", "", ""
+	g.gameBoard[1][0], g.gameBoard[1][1], g.gameBoard[1][2] = "", "", ""
+	g.gameBoard[2][0], g.gameBoard[2][1], g.gameBoard[2][2] = "", "", ""
+	g.round = 0
+	if g.alter == 0 {
+		g.playing = "X"
+		g.alter = 1
+	} else if g.alter == 1 {
+		g.playing = "O"
+		g.alter = 0
+	}
+	g.win = ""
+	g.state = 1
+}
+
+// I didn't had better ideas
+func (g *Game) CheckWin() {
+	if (g.gameBoard[0][0] == "O" && g.gameBoard[0][1] == "O" && g.gameBoard[0][2] == "O") || (g.gameBoard[1][0] == "O" && g.gameBoard[1][1] == "O" && g.gameBoard[1][2] == "O") || (g.gameBoard[2][0] == "O" && g.gameBoard[2][1] == "O" && g.gameBoard[2][2] == "O") || (g.gameBoard[0][0] == "O" && g.gameBoard[1][0] == "O" && g.gameBoard[2][0] == "O") || (g.gameBoard[0][1] == "O" && g.gameBoard[1][1] == "O" && g.gameBoard[2][1] == "O") || (g.gameBoard[0][2] == "O" && g.gameBoard[1][2] == "O" && g.gameBoard[2][2] == "O") || (g.gameBoard[0][0] == "O" && g.gameBoard[1][1] == "O" && g.gameBoard[2][2] == "O") || (g.gameBoard[0][2] == "O" && g.gameBoard[1][1] == "O" && g.gameBoard[2][0] == "O") {
+		g.win = "O"
+		g.pointsO++
+		g.state = 2
+	} else if (g.gameBoard[0][0] == "X" && g.gameBoard[0][1] == "X" && g.gameBoard[0][2] == "X") || (g.gameBoard[1][0] == "X" && g.gameBoard[1][1] == "X" && g.gameBoard[1][2] == "X") || (g.gameBoard[2][0] == "X" && g.gameBoard[2][1] == "X" && g.gameBoard[2][2] == "X") || (g.gameBoard[0][0] == "X" && g.gameBoard[1][0] == "X" && g.gameBoard[2][0] == "X") || (g.gameBoard[0][1] == "X" && g.gameBoard[1][1] == "X" && g.gameBoard[2][1] == "X") || (g.gameBoard[0][2] == "X" && g.gameBoard[1][2] == "X" && g.gameBoard[2][2] == "X") || (g.gameBoard[0][0] == "X" && g.gameBoard[1][1] == "X" && g.gameBoard[2][2] == "X") || (g.gameBoard[0][2] == "X" && g.gameBoard[1][1] == "X" && g.gameBoard[2][0] == "X") {
+		g.win = "X"
+		g.pointsX++
+		g.state = 2
+	} else if g.round == 8 {
+		g.win = "No one"
+		g.state = 2
+	}
+}
+
+func (g *Game) ResetPoints() {
+	g.pointsO = 0
+	g.pointsX = 0
 }
 
 func init() {
@@ -122,7 +190,7 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+	normalText, err = opentype.NewFace(tt, &opentype.FaceOptions{
 		Size:    fontSize,
 		DPI:     dpi,
 		Hinting: font.HintingFull,
@@ -130,6 +198,19 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	bigText, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    bigFontSize,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func newRandom() *rand.Rand {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	return rand.New(s1)
 }
 
 func main() {
